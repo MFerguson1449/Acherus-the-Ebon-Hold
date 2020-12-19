6b616bb7c753260c91af12eb7f2683c9b368d7df
MFerguson1449 <MFerguson1449@gmail.com>
2020-12-19 16:55:05 -0500

    Started Frost Guide refactor

--- dist/icon/manifest.js
+++ dist/icon/manifest.js
@@ -152,7 +152,7 @@ module.exports = {
     "density": "4.0"
   }]
 };
-},{}],"C:/Users/lukar/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{}],"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -180,7 +180,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "1389" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53241" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -355,5 +355,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["C:/Users/lukar/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","icon/manifest.json"], null)
+},{}]},{},["../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","icon/manifest.json"], null)
 //# sourceMappingURL=/icon/manifest.js.map
