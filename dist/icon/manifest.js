b2ea86a84b97edfd2aa417d6f73740ee627f6f67
MFerguson1449 <MFerguson1449@gmail.com>
2021-02-05 00:45:57 -0500

    added divs for frost guide

--- dist/icon/manifest.js
+++ dist/icon/manifest.js
@@ -152,7 +152,7 @@ module.exports = {
     "density": "4.0"
   }]
 };
-},{}],"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{}],"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -180,7 +180,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50724" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51235" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -211,8 +211,9 @@ if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
         assetsToAccept.forEach(function (v) {
           hmrAcceptRun(v[0], v[1]);
         });
-      } else {
-        window.location.reload();
+      } else if (location.reload) {
+        // `location` global exists in a web worker context but lacks `.reload()` function.
+        location.reload();
       }
     }
 
@@ -355,5 +356,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","icon/manifest.json"], null)
+},{}]},{},["../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","icon/manifest.json"], null)
 //# sourceMappingURL=/icon/manifest.js.map
